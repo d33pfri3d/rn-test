808031e4788655b01fd0ae72e3186fa796958b80
Shaun D <shaun@deepfriedlabs.com>
2023-03-22 11:35:18 +0000

    Initial commit with some basic functions

--- App.tsx
+++ App.tsx
@@ -1,11 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import "@ethersproject/shims";
+import { ethers } from 'ethers';
+import React from 'react';
+
+export function ImportWallet() {
+  const [mneumonic, setMneumonic] = useState<string>('');
+  const [wallet, setWallet] = useState<ethers.HDNodeWallet>();
+  const [error, setError] = useState<string>();
+
+  const handleSeedPhraseChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMneumonic(event.target.value);
+  };
+
+  const importWalletFromSeedPhrase = async () => {
+    try {
+      const wallet = ethers.Wallet.fromPhrase(mneumonic);;
+      setWallet(wallet);
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
+
+  return (
+    <View>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '100%' }} value={mneumonic} onChange={() => handleSeedPhraseChange} />
+      <button onClick={importWalletFromSeedPhrase}>Import wallet</button>
+      {wallet && (
+        <View>
+          <p>Wallet address: {wallet.address}</p>
+          <p>Private key: {wallet.privateKey}</p>
+        </View>
+      )}
+      {error && <p>{error}</p>}
+      <Button
+        title="Import Wallet"
+        onPress={() => {
+          console.log(mneumonic);
+        }}
+      />
+    </View>
+  )
+};
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ImportWallet />
     </View>
   );
 }
